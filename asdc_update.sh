a558d7e092343393c5f7b52e6557f652d60ed2c1
Owen Kaluza <owen.kaluza@monash.edu>
2020-09-22 16:11:54 +1000

    Added update option to resize webapp storage volume
    If WEBAPP_VOLUME_SIZE increased in settings.env, this action can be run
    to expand the active volume on a running cluster with minimal downtime:
    ./asdc_update.sh web-storage-resize

--- asdc_update.sh
+++ asdc_update.sh
@@ -33,6 +33,31 @@ then
   echo "webapp-worker deleted successfully, running asdc_run.sh to re-create and initialise the webapp pod..."
   source asdc_run.sh
 
+elif [ $1 = "web-storage-resize" ];
+then
+
+  #Resize the web-storage volume (experimental)
+  # first set the new volume size in settings.env and run 'source settings.env'
+  kubectl delete pod webapp-worker
+  kubectl delete pvc webapp-claim0
+  kubectl delete pv webapp-volume
+
+  openstack volume set --size ${WEBAPP_VOLUME_SIZE} web-storage
+
+  #Re-apply the volume sizes to volumes/claims
+  export WEB_VOLUME_ID=$(openstack volume show web-storage -c id -f value)
+  cat templates/webapp-persistentvolume.yaml | envsubst > webapp-persistentvolume.yaml
+  cat templates/webapp-persistentvolumeclaim.yaml | envsubst > webapp-persistentvolumeclaim.yaml
+
+  #Re-create with resizer pod
+  #(runs privileged and uses resize2fs to resize the ext4 fs)
+  kubectl create -f resize-webapp-volume.yaml,webapp-persistentvolume.yaml,webapp-persistentvolumeclaim.yaml
+
+  sleep 5
+
+  echo "running asdc_run.sh to re-create and initialise the webapp pod..."
+  source asdc_run.sh
+
 elif [ $1 = "ip" ];
 then
   #Need to clear port on our floating ip or it will be deleted with the cluster
