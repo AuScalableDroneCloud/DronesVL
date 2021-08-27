afba35466e95b2ae44bd863e8a0744b6405ff069
Owen Kaluza <owen.kaluza@monash.edu>
2021-08-27 11:55:47 +1000

    install.sh to use requirements.txt, moved from settings.env
    Still checking for required utilities in settings.env but don't run the
    installation.

--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
-PYPACKAGES="python-openstackclient python-novaclient python-heatclient python-magnumclient python-manilaclient"
-#PYPACKAGES="python-openstackclient python-novaclient python-heatclient python-magnumclient python-manilaclient python-keystoneclient"
-
 #Install openstack python packages, use virtual env or user 
 if env |grep VIRTUAL_ENV;
 then
   echo "pip installing in virtualenv $VIRTUAL_ENV"
-  pip install $PYPACKAGES
+  pip install -r requirements.txt
 else
   echo "pip installing with --user"
-  pip install --user $PYPACKAGES
+  pip install --user -r requirements.txt
 fi
 
-#Install kubectl?
-if [ ! -f ./kubectl ] && ! which kubectl;
+#Install kubectl + helm if not present
+if ! command -v kubectl &> /dev/null
 then
-  # https://kubernetes.io/docs/tasks/tools/install-kubectl/
-  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-
-  chmod +x ./kubectl
+  #Add cwd to path so kubectl can be run without dir
+  PATH=$PATH:$(pwd)
+  if ! command -v kubectl &> /dev/null
+  then
+    echo "kubectl could not be found! attempting to download..."
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x ./kubectl
+  fi
 fi
 
+#Install helm if not found
+if ! command -v helm &> /dev/null
+then
+  echo "helm could not be found! attempting to download..."
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+  chmod 700 get_helm.sh
+  ./get_helm.sh
+fi
