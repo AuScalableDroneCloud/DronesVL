ab7eaf15d44c75f0c0732733a3b6b911d4c7e14c
Owen Kaluza <owen.kaluza@monash.edu>
2022-07-13 13:08:09 +1000

    Fix/clean up stop script
    Was not using $KUBECONFIG for config file location

--- asdc_stop.sh
+++ asdc_stop.sh
@@ -15,46 +15,9 @@ then
   exit
 fi
 
-#Need to clear port on our floating ip or it will be deleted with the cluster
-#./asdc_update.sh ip
-
-#Same for tusd service ip - deprecated
-#./asdc_update.sh ip tusd
-
 #Delete the cluster
 openstack coe cluster delete $CLUSTER
-function delete_volume()
-{
-  echo "Deleting volume $1"
-  VOL_ID=$(openstack volume show $1 -f value -c id)
-  if [ $? -eq 0 ];
-  then
-    openstack volume delete $VOL_ID
-  fi
-}
 
 #Remove the config file
-rm secrets/kubeconfig
-
-###DON'T DELETE VOLUMES UNLESS STARTING FROM SCRATCH
-exit
-####################################################################################################
-
-#TODO: wait until DELETE_IN_PROGRESS over / cluster gone
-echo "Waiting"
-sleep 10
-
-#Deleting test volumes - in production we would not do this!
-delete_volume web-storage
-delete_volume db-storage
-
-for (( n=1; n<=$NODES; n++ ))
-do
-  delete_volume nodeodm$n-storage
-done
-
-delete_volume clusterodm-storage
-delete_volume nodemicmac-storage
-unset KUBECONFIG
-unset STACK_ID
+rm ${KUBECONFIG
 
